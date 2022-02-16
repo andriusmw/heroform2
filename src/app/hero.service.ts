8e1e1b9d668d2f4a50626444bb22b034fbbd6064
andriusmw <andriusmw@yahoo.es>
2022-02-16 16:05:02 +0100

    heroe por defecto

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,7 +5,7 @@ export class Hero {
     public id: number,
     public name: string,
     public power: string,
-    public alterEgo?: string
+    public alterEgo: string
 
 
   ) { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../hero.service'; //importamos el servicio de
+//hero.service
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+
+
+  hero : Hero = {
+   id: 42,
+   name: "SkyDog",
+   power: "Fetch any object at any distance",
+  alterEgo: "Leslie Rollover"
+
+
+
+
+};
 
   constructor() { }
 
