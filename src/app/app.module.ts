37fe337f4483cb09b6f6a0a3db46025e83574325
andriusmw <andriusmw@yahoo.es>
2022-02-16 16:13:34 +0100

    heroe por defecto SE MUESTRA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroFormComponent } from './hero-form/hero-form.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { HeroFormComponent } from './hero-form/hero-form.component'; //añadido para no dar fallo en app.component.html
+import { HeroesComponent } from './heroes/heroes.component'; //añadido para no dar fallo en app.component.html
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroFormComponent,
-    HeroesComponent
+    HeroFormComponent, //añadido para no dar fallo en app.component.html
+    HeroesComponent //añadido para no dar fallo en app.component.html
   ],
   imports: [
     BrowserModule,
