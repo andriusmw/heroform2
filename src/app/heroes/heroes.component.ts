ecb0defa6cd42fa0da11bed21ddc622ce582d46b
andriusmw <andriusmw@yahoo.es>
2022-02-16 16:36:17 +0100

    Mostramos la lista de héroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.service'; //importamos el servicio de
 //hero.service
+import { HEROES } from '../mock-heroes-list';  //Cargamos la lista de héroes
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,7 +9,7 @@ import { Hero } from '../hero.service'; //importamos el servicio de
 })
 export class HeroesComponent implements OnInit {
 
-
+  heroes = HEROES; //llama a al array de héroes de clase Hero.
 
   hero : Hero = {
    id: 42,
